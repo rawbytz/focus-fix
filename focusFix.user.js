8c374e2e9ff494542f127c38c7ac7f7eab2a7e7a
rawbytz <rawbytz@gmail.com>
2020-08-17 10:23:50 -0600

    added comments

--- focusFix.user.js
+++ focusFix.user.js
@@ -20,7 +20,7 @@
     if (active.includes("searchBoxInput") || active.includes("content")) return;
     const matches = document.querySelectorAll(".name.matches .content, .notes.matches .content, .metaMatches .name .content");
     if (matches.length > 0) return void matches[0].focus();
-    const index = WF.currentItem().isMainDocumentRoot() && !WF.currentSearchQuery() ? 2 : 0;
+    const index = WF.currentItem().isMainDocumentRoot() && !WF.currentSearchQuery() ? 2 : 0; // home name [0] and note [1] are "hidden"
     const content = document.getElementsByClassName("content");
     if (content.length > 0) content[index].focus();
     }
@@ -32,6 +32,7 @@
     if (event === "locationChanged") requestAnimationFrame(fixFocus);
   };
 
+  // Set focus on WorkFlowy initial load
   function waitForActivePage() {
     if (document.querySelector(".page.active")) return void fixFocus();
     setTimeout(waitForActivePage, 300);
